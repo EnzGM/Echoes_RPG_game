a381ab5532a93d3002a485b920638c8e1cf2df4a
Enzo Gabriel Miranda <enzoblu.ti@gmail.com>
2026-08-04 21:09:17 -0300

    Aula 5

---
+++ core/src/main/java/events/GameEvent.java
@@ -0,0 +1,33 @@
+package events;
+
+public class GameEvent {
+
+    private final EventType type;
+    private final Object data;
+
+    public GameEvent(EventType type) {
+        this.type = type;
+        this.data = null;
+    }
+
+    public GameEvent(EventType type, Object data) {
+        this.type = type;
+        this.data = data;
+    }
+
+    public EventType getType() {
+        return type;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T getdata(Class<T> clazz) {
+        if (data != null && clazz.isInstance(data)) {
+            return (T) data;
+        }
+        return null;
+    }
+}
